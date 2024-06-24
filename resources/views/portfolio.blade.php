f263aa8871a4ca3261a05537260e1c2acf7a8931
Syarif Hidayatulloh <iamsyarifh@gmail.com>
2024-06-24 23:05:13 +0700

    add image for porto 3

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -19,7 +19,7 @@
           </a>
           <a href="https://github.com/syarifhiday/FaceCare" class="group">
             <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-              <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="h-full w-full object-cover object-center group-hover:opacity-75">
+              <img src="images/porto3.png" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="h-full w-full object-cover object-center group-hover:opacity-75">
             </div>
             <p class="mt-1 text-lg font-medium text-gray-900">FaceCare - An App to Help Maintain Your Facial Health</p>
             <h3 class="mt-4 text-sm text-gray-700">Android - kotlin</h3>
