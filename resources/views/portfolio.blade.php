86916dfcb69712ae1560d2fd529b9bfeb47d5409
Syarif Hidayatulloh <iamsyarifh@gmail.com>
2024-06-24 23:00:22 +0700

    Add repo link to portfolio

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -3,28 +3,21 @@
         <h2 class="sr-only">Products</h2>
 
         <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
-          <a href="#" class="group">
+          <a href="https://github.com/syarifhiday/Aplikasi-Sistem-Jemuran-IoT" class="group">
             <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
               <img src="images/porto1.png" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="h-full w-full object-cover object-center group-hover:opacity-75">
             </div>
             <p class="mt-1 text-lg font-medium text-gray-900">IoT clothesline system controller application</p>
             <h3 class="mt-4 text-sm text-gray-700">Android - kotlin</h3>
           </a>
-          <a href="#" class="group">
+          <a href="https://github.com/syarifhiday/Aplikasi-Asisten-Kopi" class="group">
             <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
               <img src="images/porto2.png" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="h-full w-full object-cover object-center group-hover:opacity-75">
             </div>
             <p class="mt-1 text-lg font-medium text-gray-900">IoT coffee machine controller application</p>
             <h3 class="mt-4 text-sm text-gray-700">Android - kotlin</h3>
           </a>
-          <a href="#" class="group">
-            <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-              <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="h-full w-full object-cover object-center group-hover:opacity-75">
-            </div>
-            <p class="mt-1 text-lg font-medium text-gray-900">Blog</p>
-            <h3 class="mt-4 text-sm text-gray-700">Website - Codeigniter</h3>
-          </a>
-          <a href="#" class="group">
+          <a href="https://github.com/syarifhiday/FaceCare" class="group">
             <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
               <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="h-full w-full object-cover object-center group-hover:opacity-75">
             </div>
